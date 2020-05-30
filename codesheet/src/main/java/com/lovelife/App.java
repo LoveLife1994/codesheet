32cd95d7e6b2b3dc8d814d0c006ebc48208d2e7c
lovelife <lovelife@lovelifedeMBP.lan>
2020-05-30 21:49:55 +0800

    init project

---
+++ codesheet/src/main/java/com/lovelife/App.java
@@ -0,0 +1,13 @@
+package com.lovelife;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
